7a2455947beec73a0fddacb56b1b944acde4283a
katerebello <katerebello27@gmail.com>
2024-11-11 11:33:53 -0800

    created basic flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+# @app.route('/<name>')
+def index():
+    return '<h1>Hello World!</h1>'
+    # return '<h1>Hello {}!</h1>'.format(name)
+
+
