70fb2af643e456af02a8f25872608ee11767e0c7
katerebello <katerebello27@gmail.com>
2024-11-11 19:17:02 -0800

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 # @app.route('/<name>')
 def index():
-    return '<h1>Hello World Updated!</h1>'
+    return '<h1>Hello World!</h1>'
     # return '<h1>Hello {}!</h1>'.format(name)
 
 
